336bc8867c2639b672fe0afbe22ec82ddc7e496e
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2024-05-25 22:39:30 -0700

    Add 01-07 solutions

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let splitString = string.split("");
+  let reversedSplitString = splitString.reverse();
+  return reversedSplitString.join('');
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+  return array.filter(e => !args.includes(e))
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
